5d87148acfcca52c64b371023e935ad67a6faa18
Guilherme Epifânio <115120496+Guiscoob7@users.noreply.github.com>
2026-07-30 04:33:51 -0300

    Refactor Calculadora methods and add constants
    Refactor discount and tax calculations to use constants.

--- src/main/java/com/workshop/Calculadora.java
+++ src/main/java/com/workshop/Calculadora.java
@@ -1,31 +1,43 @@
 package com.workshop;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.io.File;
-
 public class Calculadora {
 
-    public int X;
+    private static final double LIMITE_DESCONTO = 100;
+    private static final double PERCENTUAL_DESCONTO = 0.9;
+    private static final double ALIQUOTA_IMPOSTO = 0.27;
+
+    private int x;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
 
     public int soma(int a, int b) {
         return a + b;
     }
 
-    public double CalcularDesconto(double preco) {
-        if (preco > 100) return preco * 0.9;
-        else return preco;
+    public double calcularDesconto(double preco) {
+        if (preco > LIMITE_DESCONTO) {
+            return preco * PERCENTUAL_DESCONTO;
+        } else {
+            return preco;
+        }
     }
 
     public double calcularImposto(double valor) {
-        double resultado = valor * 0.27;
+        double resultado = valor * ALIQUOTA_IMPOSTO;
         return resultado;
     }
 
     public int fatorial(int n) {
         int r = 1;
-        for (int i = 1; i <= n; i++) r = r * i;
+        for (int i = 1; i <= n; i++) {
+            r = r * i;
+        }
         return r;
     }
 }
